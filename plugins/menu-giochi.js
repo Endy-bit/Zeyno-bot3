480d331c5ea5d4e27a299a8937c267fb68186202
Endy-bit <endy81762@gmail.com>
2026-04-11 11:40:31 +0200

    Update menu-giochi.js

--- plugins/menu-giochi.js
+++ plugins/menu-giochi.js
@@ -1,6 +1,6 @@
 import { performance } from 'perf_hooks';
 
-const handler = async (message, { conn, usedPrefix = '.' }) => {
+const handler = async (message, { conn, usedPrefix = '!' }) => {
 
     const userId = message.sender;
     const uptimeMs = process.uptime() * 1000;
@@ -8,7 +8,7 @@ const handler = async (message, { conn, usedPrefix = '.' }) => {
     const totalUsers = Object.keys(global.db?.data?.users || {}).length;
 
     const menuBody = `
-『 𝚫𝐗𝐈𝐎𝐍 • 𝐆𝐀𝐌𝐄 』
+『 ZΞYNΩ BΩT • 𝐆𝐀𝐌𝐄 』
 ╼━━━━━━━━━━━━━━╾
   ◈ *ᴜsᴇʀ:* @${userId.split('@')[0]}
   ◈ *ᴜᴘᴛɪᴍᴇ:* ${uptimeStr}
@@ -69,7 +69,7 @@ const handler = async (message, { conn, usedPrefix = '.' }) => {
 ╰━━━━━━━━━━━━━━━━⬣
 
 ╭━━━〔 📌 ɪɴғᴏ 〕━⬣
-┃ ᴠᴇʀsɪᴏɴᴇ: 1.0
+┃ ᴠᴇʀsɪᴏɴᴇ: 5.0
 ┃ sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ⚡
 ╰━━━━━━━━━━━━━━━━⬣
 `.trim();
